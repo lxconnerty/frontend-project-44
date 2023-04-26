961364a067132296048f3fcf6006af2fe0c388b8
lxconnerty <kolunovmaks5@gmail.com>
2023-04-26 18:21:36 +0300

    2 game complete

--- bin/brain-even.js
+++ bin/brain-even.js
@@ -12,29 +12,11 @@ const check = (num) => {
 
 let count = 0
 const evenGame = () => {
-    
     console.log('Answer "yes" if the number is even, otherwise answer "no".')
     while(count <= 3){
-        
         console.log(`Question: ${randomNumber()}`)
         const correctAns = check(randomNumber()) ? 'yes' : 'no'
         const answer = readlineSync.question('Answer: ')
-        // if(answer === 'yes' & check(answer)){
-        //     console.log('Correct!')
-        //     count++
-        // }else{
-        //     console.log(`'yes' is wrong answer ;(. Correct answer was 'no'.
-        //     Let's try again, ${name}!`)
-        //     break
-        // }
-        // if(answer === 'no' & !check(answer)){
-        //     console.log('Correct!')
-        //     count++
-        // }else{
-        //     console.log(`'no' is wrong answer ;(. Correct answer was 'yes'.
-        //     Let's try again, ${name}!`)
-        //     break
-        // }
         if(answer === correctAns){
             console.log('Correct!')
             count++
