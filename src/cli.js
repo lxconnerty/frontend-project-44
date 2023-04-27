d3053ab021c7c376a0d8a280c23fbee0924ab770
lxconnerty <kolunovmaks5@gmail.com>
2023-04-27 21:33:19 +0300

    add , in cli......

--- src/cli.js
+++ src/cli.js
@@ -3,6 +3,6 @@ import readlineSync from 'readline-sync';
 
 export default function greeting () {
   const name = readlineSync.question('May I have your name?: ');
-  console.log(`Hello ${name}!`);
+  console.log(`Hello, ${name}!`);
   return name;
 };
